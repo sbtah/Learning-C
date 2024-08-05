0aeda613198c818190a04f8d61708a7d924e50fc
Sbtah <shisastore@protonmail.com>
2024-08-05 22:47:40 +0200

    Progress...

---
+++ Beginer/practice_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("This is my program...!\n");
+
+    int x = 0;
+
+    for (x = 0; x <= 10; x++) {
+
+        printf("Current val of x: %d\n", x);
+
+    }
+
+    return 0;
+
+}
---
+++ Beginer/practice_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int i = 1;
+
+    while (1) {
+        
+        printf("Current i: %d\n", i);
+
+        i += 1;
+
+    }
+
+}
